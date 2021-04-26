578f8e8aec76ac366f63f3f7d67afdd65f2cd40f
David <davidirvin47@gmail.com>
2021-04-25 21:45:24 -0600

    adds animation to value cards

--- home.js
+++ home.js
@@ -1,24 +1,80 @@
 let options = {
     root: null,
-    rootMargin: "10% 0%",
+    rootMargin: "-10% 0%",
     threshold: 1
-
 }
 
-let callback = (entries, observer) => {
+let options2 = {
+    root: null,
+    rootMargin: "-10% 0%",
+    threshold: 0.8
+}
+
+
+let valuesCallback = (entries, observer) => {
     entries.forEach(entry => {
         if(entry.isIntersecting === true) {
             entry.target.style.animation = "zoom-in 1000ms ease forwards"
         }
+        // if(entry.isVisible === false && entry.isIntersecting === false) {
+        //     entry.target.style.animation = ""
+        // }
+    })
+}
+
+let values = new IntersectionObserver(valuesCallback, options);
+
+
+values.observe(document.querySelector(".values"))
+
+
+// let valueTextCallback = (entries) => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting === true) {
+//             entry.target.style.animation = "slide-from-right 1000ms ease forwards"
+//         }
+//         if(entry.isVisible === false && entry.isIntersecting === false) {
+//             entry.target.style.animation = "fade-in 500ms ease reverse"
+//         }
+//     })
+// }
+
+// let valueImgCallback = (entries) => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting === true) {
+//             entry.target.style.animation = "slide-from-left 1000ms ease forwards"
+//         }
+//         if(entry.isVisible === false && entry.isIntersecting === false) {
+//             entry.target.style.animation = "fade-in 500ms ease reverse"
+//         }
+//     })
+// }
+
+const valueCardCallback = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting === true) {
+            entry.target.children[0].style.animation = "slide-from-left 1000ms ease forwards"
+            entry.target.children[1].style.animation = "slide-from-right 1000ms ease forwards"
+        }
         if(entry.isVisible === false && entry.isIntersecting === false) {
-            entry.target.style.animation = ""
+            var children = Array.from(entry.target.children)
+            children.forEach(child => {
+                child.style.animation = "fade-in 1500ms ease reverse"
+            })
         }
     })
 }
 
-let observer = new IntersectionObserver(callback, options);
+// const valueImgs = Array.from(document.querySelectorAll(".img--container"))
+// let valueImg = new IntersectionObserver(valueImgCallback, options2)
 
+// valueImgs.forEach(img => {valueImg.observe(img)})
 
+// const valueTexts = Array.from(document.querySelectorAll(".introduction--card-text"))
+// let valueText = new IntersectionObserver(valueTextCallback, options2)
 
+// valueTexts.forEach(text => {valueText.observe(text)})
 
-observer.observe(document.querySelector(".values"))
+const valueCards = Array.from(document.querySelectorAll(".introduction--card"))
+const valueCard = new IntersectionObserver(valueCardCallback, options2)
+valueCards.forEach(card => valueCard.observe(card))
