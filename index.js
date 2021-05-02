4fcb07c4718bfc08363c8ed1e4d72a426ecccf67
David <davidirvin47@gmail.com>
2021-05-01 18:30:05 -0600

    Creates external file to mock a database
    Can't do it currently with this iteration of the code.  Need to refactor
    to React so babel can allow import of JSON file. Currently it is just a
    js object in a new file.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const inventory = [
-    {item: "Apples", imgSrc: "./images/apples.jpg", price: .99, discount: {quantity: 2, price: .79}},
-    {item: "Bananas", imgSrc: "./images/bananas.jpg", price: .99},
-    {item: "Oranges", imgSrc: "./images/oranges.jpg", price: .99},
-    {item: "Onions", imgSrc: "./images/onions.jpg", price: .99, discount: {quantity: 4, price: .79}},
-    {item: "Carrots", imgSrc: "./images/carrots.jpg", price: .99},
-    {item: "Celery", imgSrc: "./images/celery.jpg", price: .99}
-]
+// const inventory = [
+//     {item: "Apples", imgSrc: "./images/apples.jpg", price: .99, discount: {quantity: 2, price: .79}},
+//     {item: "Bananas", imgSrc: "./images/bananas.jpg", price: .99},
+//     {item: "Oranges", imgSrc: "./images/oranges.jpg", price: .99},
+//     {item: "Onions", imgSrc: "./images/onions.jpg", price: .99, discount: {quantity: 4, price: .79}},
+//     {item: "Carrots", imgSrc: "./images/carrots.jpg", price: .99},
+//     {item: "Celery", imgSrc: "./images/celery.jpg", price: .99}
+// ]
+
 
 const shoppingSection = document.querySelector(".inventory")
 const cartSection = document.querySelector(".cart")
---
+++ inventory.js
@@ -0,0 +1,40 @@
+const inventory = [
+    {
+        item: "Apples", 
+        imgSrc: "./images/apples.jpg", 
+        price: .99, 
+        discount: {
+            quantity: 2, 
+            price: .79
+        }
+    },
+    {
+        item: "Bananas", 
+        imgSrc: "./images/bananas.jpg", 
+        price: .99
+    },
+    {
+        item: "Oranges", 
+        imgSrc: "./images/oranges.jpg", 
+        price: .99
+    },
+    {
+        item: "Onions", 
+        imgSrc: "./images/onions.jpg", 
+        price: .99, 
+        discount: {
+            quantity: 4, 
+            price: .79
+        }
+    },
+    {
+        item: "Carrots", 
+        imgSrc: "./images/carrots.jpg", 
+        price: .99
+    },
+    {
+        item: "Celery", 
+        imgSrc: "./images/celery.jpg", 
+        price: .99
+    }
+]
